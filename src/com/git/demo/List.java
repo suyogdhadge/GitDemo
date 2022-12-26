0592d75a2fb3056a839d505a0d3e40c88a5204b2
suyogdhadge <dhadgesuyog@gmail.com>
2022-12-26 23:25:12 +0530

    second commit add new class and list

---
+++ src/com/git/demo/List.java
@@ -0,0 +1,19 @@
+package com.git.demo;
+
+import java.util.ArrayList;
+
+
+public class List {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add("ram");
+		al.add("shyam");
+		al.add("rahul");
+		al.add("amit");
+		al.add("raj");
+		al.add("abhishek");
+		
+		System.out.println(al);
+	}
+}
